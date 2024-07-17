19675046fb597d24b3d0dc454b65bc1d775b08a1
VladimirKataev <vova.kataev@gmail.com>
2024-07-17 15:16:05 -0700

    highlight mouseover and mouseout

--- docs/script/reader.js
+++ docs/script/reader.js
@@ -11,15 +11,28 @@ function generate(){ // user presses "generate table", table is generated with t
     nr = document.getElementById("nRows").value;
     nc = document.getElementById("nCols").value;
 
-    var tableText = "";
-    for(let r = 0; r < nr; r++){
+    var tableText = "<tr style='color:blue;'>";
+    for(let c = 0; c < nc; c++){
+        tableText += `<th> ${String.fromCharCode(65 + c)} </th>`;
+    }
+    tableText += "</tr>";
+    for(let r = 1; r <= nr; r++){
         tableText += "<tr>";
         for(let c = 0; c < nc; c++){
-            tableText += "<td>O</td> "; 
+            tableText += `<td id = "b${r}-${c}" onmouseover="mousedOver(${r}, ${c})" onmouseout="mousedOut(${r}, ${c})"> O</td> `; 
         }
         tableText += "</tr>\n";
     }
     console.log(tableText);
     document.getElementById("mainTable").innerHTML = tableText;
 
+}
+
+function mousedOver(u, v){
+    console.log(u, v);
+    document.getElementById(`b${u}-${v}`).style.color = "red";
+
+}
+function mousedOut(u,v){
+    document.getElementById(`b${u}-${v}`).style.color = "black";
 }
